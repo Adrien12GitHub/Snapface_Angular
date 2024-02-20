a0a3adbaf7bbbb64af9b4d07d964f7337c6d3ebd
Adrien Fepessi <adrienfepessi1@gmail.com>
2024-02-20 20:58:09 +0100

    add a routing module and 3 functional routes to the application

--- snapface/src/app/services/face-snaps-service.ts
+++ snapface/src/app/services/face-snaps-service.ts
@@ -7,32 +7,50 @@ import { FaceSnap } from '../models/face-snap.model';
 export class FaceSnapsService {
   faceSnaps: FaceSnap[] = [
     {
-        id: 1,
-        title: 'Archibald',
-        description: 'Mon meilleur ami depuis tout petit !',
-        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-        createdDate: new Date(),
-        snaps: 200,
-        location: "Berlin"
-      },
-      {
-        id: 2,
-        title: 'Three Rock Mountain',
-        description: 'Un endroit magnifique pour les randonnées.',
-        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
-        createdDate: new Date(),
-        snaps: 5,
-        location: "Frankfurt"
-      },
-      {
-        id: 3,
-        title: 'Un bon repas',
-        description: 'Mmmh que c\'est bon !',
-        imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
-        createdDate: new Date(),
-        snaps: 0,
-        location: "Stuttgart"
-      }
+      id: 1,
+      title: 'Archibald',
+      description: 'Mon meilleur ami depuis tout petit !',
+      imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+      createdDate: new Date(),
+      snaps: 200,
+      location: "Berlin"
+    },
+    {
+      id: 2,
+      title: 'Three Rock Mountain',
+      description: 'Un endroit magnifique pour les randonnées.',
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
+      createdDate: new Date(),
+      snaps: 5,
+      location: "Dublin"
+    },
+    {
+      id: 3,
+      title: 'Un bon repas',
+      description: 'Mmmh que c\'est bon !',
+      imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
+      createdDate: new Date(),
+      snaps: 10,
+      location: "Stuttgart"
+    },
+    {
+      id: 4,
+      title: 'Mon université',
+      description: 'Je vous recommande cette école supérieure.',
+      imageUrl: 'https://sphere-emotion.fr/wp-content/uploads/2015/10/belle-universite-monde-28.jpg',
+      createdDate: new Date(),
+      snaps: 45,
+      location: "Portugal"
+    },
+    {
+      id: 5,
+      title: 'Un site touristique',
+      description: 'Woww que c\'est beau !',
+      imageUrl: 'https://content.r9cdn.net/rimg/dimg/ac/93/63e66ec2-lm-943-16ac6629cb7.jpg',
+      createdDate: new Date(),
+      snaps: 15,
+      location: "Paris"
+    }
   ];
   
   getAllFaceSnaps(): FaceSnap[] {
